a7db22201e28efea70cc04583c27d5ac092b7490
AugustDruzgal <august.druzgal@Knights.ucf.edu>
2025-10-09 16:35:08 -0400

    Lab 2 week 3 work

--- ECE4760/Lab_2/animation.c
+++ ECE4760/Lab_2/animation.c
@@ -53,6 +53,7 @@ typedef signed int fix15;
 #define char2fix15(a) (fix15)(((fix15)(a)) << 15)
 #define divfix(a,b) (fix15)(div_s64s64( (((signed long long)(a)) << 15), ((signed long long)(b))))
 
+// Screen boundaries
 #define SCREEN_TOP_Y 5
 #define SCREEN_BOTTOM_Y 475
 #define SCREEN_LEFT_X 5
@@ -66,53 +67,86 @@ typedef signed int fix15;
 
 // uS per frame
 #define FRAME_RATE 33000
+#define VSYNC 17
 
 // the color of the ball
 char color = WHITE;
 
-typedef struct _ball {
-	fix15 x;
-	fix15 y;
-	fix15 vx;
-	fix15 vy;
-	int last_peg;
-} ball_t;
-
+// Peg definition
 typedef struct _peg {
 	fix15 x;
 	fix15 y;
 } peg_t;
 
+// Ball definition
+typedef struct _ball {
+	fix15 x;
+	fix15 y;
+	fix15 vx;
+	fix15 vy;
+	peg_t *last_peg;
+} ball_t;
+
+// Button mode definitions
+typedef enum _button_mode {
+	BUTTON_MODE_NUM_BALLS,
+	BUTTON_MODE_BOUNCINESS,
+	BUTTON_MODE_GRAVITY,
+	BUTTON_MODE_NOTHING
+} button_mode;
+
+// Button state machine states
+typedef enum _button_state {
+    BUTTON_NOT_PRESSED,
+    BUTTON_MAYBE_PRESSED,
+    BUTTON_PRESSED,
+    BUTTON_MAYBE_NOT_PRESSED
+} button_state;
+
+// Ball configuration
 #define MIN_BALLS 1
-#define MAX_BALLS 256
-#define BALL_RADIUS 2
-#define BALL_BOUNCINESS 0.45f
-#define BALL_GRAVITY 0.75f
-#define BALL_START_DX_MAX 1.0f
+#define MAX_BALLS 4096
+#define BALL_RADIUS_INT 1
+#define BALL_RADIUS_FIX int2fix15(BALL_RADIUS_INT)
+#define MAX_GRAVITY 2.0f
+#define MIN_GRAVITY 0.1f
+#define MAX_BOUNCINESS 0.9f
+#define MIN_BOUNCINESS 0.1f
+#define BALL_START_DX_MAX 1
 #define BALL_START_X 320
 #define BALL_START_Y 25
+
+// Ball array
 static ball_t balls[MAX_BALLS];
 static int balls_len = 0;
 
+// Galton board configuration
 #define GALTON_HEIGHT 16
 #define GALTON_MAX_HEIGHT 16
 #define GALTON_H_SEPARATION 38
 #define GALTON_V_SEPARATION 19
 #define GALTON_TOP_Y 50
-#define GALTON_BOTTOM_Y GALTON_TOP_Y + (GALTON_HEIGHT - 1) * GALTON_V_SEPARATION + PEG_RADIUS
+#define GALTON_BOTTOM_Y GALTON_TOP_Y + (GALTON_HEIGHT - 1) * GALTON_V_SEPARATION + PEG_RADIUS_INT
 
+// Graph configuration
 #define GRAPH_LEN GALTON_HEIGHT + 1
 #define GRAPH_START_Y SCREEN_BOTTOM_Y
 #define GRAPH_MAX_Y   GALTON_BOTTOM_Y + 10
 #define GRAPH_BAR_WIDTH 30
-
 #define MAX_PEGS GALTON_MAX_HEIGHT * GALTON_MAX_HEIGHT
-#define PEG_RADIUS 6
+#define PEG_RADIUS_INT 6
+#define PEG_RADIUS_FIX int2fix15(PEG_RADIUS_INT)
 
+// Peg array
 static peg_t pegs[MAX_PEGS];
 static int pegs_len = 0;
-static uint32_t graph[GRAPH_LEN];
 
+// Graph array
+static int graph[GRAPH_LEN];
+
+// Simulation parameters
+static fix15 ball_gravity = float2fix15(0.75f);
+static fix15 ball_bounciness = float2fix15(0.45f);
 static float time_since_boot = 0;
 static uint32_t num_fallen_through = 0;
 
@@ -128,6 +162,7 @@ unsigned short DAC_data[sine_table_size];
 // Pointer to the address of the DAC data table
 unsigned short * _address_pointer = &DAC_data[0];
 
+// DMA channels
 static int data_chan;
 static int ctrl_chan;
 
@@ -141,24 +176,118 @@ static int ctrl_chan;
 #define PIN_CS   5
 #define SPI_PORT spi0
 
+// LED, button, and potentiometer configuration
+#define LED 25
 #define PIN_POT 27
 #define POT_MAX 4000
-#define POT_MIN_DEVIATION 20
+#define POT_MIN_DEVIATION 40
+#define BUTTON_PIN 10
 
+// Potentiometer variables
 static bool num_balls_set = true;
 static int pot_last_value = 0;
+static button_mode mode = BUTTON_MODE_NOTHING;
+static button_state state = BUTTON_NOT_PRESSED;
 
 // Number of DMA transfers per event
 const uint32_t transfer_count = sine_table_size;
 
-uint16_t readPotentiometer()
+// Read the current value from the potentiometer
+uint16_t readPotentiometer(void)
 {
 	return adc_read();
 }
 
+// Convert the button mode to a string
+char *button_mode_to_str(button_mode _mode)
+{
+	switch (mode)
+	{
+		case BUTTON_MODE_NUM_BALLS: 
+			return "Ball Count ";
+		case BUTTON_MODE_BOUNCINESS: 
+			return "Bounciness ";
+		case BUTTON_MODE_GRAVITY: 
+			return "Gravity    ";
+		case BUTTON_MODE_NOTHING:
+			return "Nothing    ";
+	}
+}
+
+// Sequentialy change the button mode
+void change_button_mode(void)
+{
+	switch (mode)
+	{
+		case BUTTON_MODE_NUM_BALLS:
+			mode = BUTTON_MODE_BOUNCINESS;
+			break;
+
+		case BUTTON_MODE_BOUNCINESS:
+			mode = BUTTON_MODE_GRAVITY;
+			break;
+
+		case BUTTON_MODE_GRAVITY:
+			mode = BUTTON_MODE_NOTHING;
+			break;
+		
+		case BUTTON_MODE_NOTHING:
+			mode = BUTTON_MODE_NUM_BALLS;
+			break;
+	}
+}
+
+// Update the state of the button using the button state machine
+void update_button_state(void)
+{
+	uint32_t pressed = (gpio_get(BUTTON_PIN) == 0);
+
+	switch (state)
+	{
+		case BUTTON_NOT_PRESSED:
+			if (pressed)
+			{
+				state = BUTTON_MAYBE_PRESSED;
+			}
+			break;
+
+		case BUTTON_MAYBE_PRESSED:
+			if (pressed)
+			{
+				// When the button enters the pressed state, change the button mode
+				state = BUTTON_PRESSED;
+				change_button_mode();
+			}
+			else
+			{
+				state = BUTTON_NOT_PRESSED;
+			}
+			break;
+
+		case BUTTON_PRESSED:
+			if (!pressed)
+			{
+				state = BUTTON_MAYBE_NOT_PRESSED;
+			}
+			break;
+
+		case BUTTON_MAYBE_NOT_PRESSED:
+			if (pressed)
+			{
+				state = BUTTON_PRESSED;
+			}
+			else
+			{
+				state = BUTTON_NOT_PRESSED;
+			}
+			break;
+	}
+}
+
+// Reset a ball's position to the top of the screen
 void resetBall(ball_t *ball)
 {
-	ball->last_peg = -1;
+	ball->last_peg = NULL;
 	ball->x = int2fix15(BALL_START_X);
 	ball->y = int2fix15(BALL_START_Y);
 	ball->vy = 0;
@@ -166,21 +295,23 @@ void resetBall(ball_t *ball)
 	int r = rand() % 1000;
 	r -= 500;
 	
-	ball->vx = multfix15(divfix(int2fix15(r), int2fix15(500)), float2fix15(BALL_START_DX_MAX));
+	ball->vx = multfix15(divfix(int2fix15(r), int2fix15(500)), int2fix15(BALL_START_DX_MAX));
 }
 
-// Create a ball
+// Create a new ball
 void spawnball(void)
 {
 	ball_t *ball = &balls[balls_len++];
 	resetBall(ball);
 }
 
+// Remove a ball from the screen
 void removeball(ball_t *ball)
 {
-	fillCircle(fix2int15(ball->x), fix2int15(ball->y), BALL_RADIUS, BLACK);
+	drawCircle(fix2int15(ball->x), fix2int15(ball->y), BALL_RADIUS_INT, BLACK);
 }
 
+// Change the current number of balls in the simulation
 void setNumBalls(uint32_t num_balls)
 {
 	if (num_balls > MAX_BALLS)
@@ -188,6 +319,7 @@ void setNumBalls(uint32_t num_balls)
 		num_balls = MAX_BALLS;
 	}
 
+	// If the new amount of balls is greater than the old one, add new balls. Otherwise, remove balls
 	if (balls_len < num_balls)
 	{
 		for (int i = balls_len; i < num_balls; i++)
@@ -199,48 +331,12 @@ void setNumBalls(uint32_t num_balls)
 	{
 		balls_len = num_balls;
 	}
-
 }
 
-void update_potentiometer(void)
-{
-	uint16_t pot_value = readPotentiometer();
-
-	if (abs(pot_last_value - (int)pot_value) < POT_MIN_DEVIATION)
-	{
-		return;
-	}
-
-	pot_last_value = pot_value;
-
-	if (pot_value > POT_MAX)
-	{
-		pot_value = POT_MAX;
-	}
-
-	pot_value = (int) (((float) pot_value / (float) POT_MAX) * ((float) MAX_BALLS));
-	
-	setNumBalls(pot_value);
-}
-
-void play_hit_sound(void)
-{
-	dma_channel_start(ctrl_chan);
-}
-
-void resetGraph(void)
-{
-	for (int i = 0; i < GRAPH_LEN; i++)
-	{
-		graph[i] = 0;
-	}
-
-	num_fallen_through = 0;
-}
-
+// Update the graph depending on the position of a ball leaving the galton board
 void updateGraph(ball_t *ball)
 {
-	int startx = 320 - (int) (((float) (GALTON_HEIGHT-1))/2.0 * GALTON_H_SEPARATION);
+	int startx = 320 - (((GALTON_HEIGHT-1)) * GALTON_H_SEPARATION)/2;
 	int x = fix2int15(ball->x);
 
 	if (x <= startx)
@@ -261,6 +357,18 @@ void updateGraph(ball_t *ball)
 	graph[GALTON_HEIGHT]++;
 }
 
+// Clear the displayed graph
+void clearGraph(void)
+{
+	int startx = 320 - (((GALTON_HEIGHT+2)) * GALTON_H_SEPARATION)/2;
+
+	for (int i = 0; i < GRAPH_LEN; i++)
+	{
+		drawRect(startx + i * GALTON_H_SEPARATION - GRAPH_BAR_WIDTH/2, GRAPH_START_Y - 100, GRAPH_BAR_WIDTH, 100, BLACK);
+	}
+}
+
+// Draw the graph on the bottom of the screen
 void drawGraph(void)
 {
 	int start_x = 320 - (int) (((float) (GALTON_HEIGHT + 2))/2.0 * GALTON_H_SEPARATION);
@@ -283,14 +391,110 @@ void drawGraph(void)
 			continue;
 		}
 
-		// proportion = ((float) graph[i] / (float) graph_max);
-		// graph_height = (int) (proportion * (float) (GRAPH_START_Y - GALTON_BOTTOM_Y));
-		graph_height = (int) ((((float) graph[i]) / ((float) graph_max)) * 100);
+		graph_height = fix2int15(multfix15(divfix(int2fix15(graph[i]),int2fix15(graph_max)), int2fix15(100)));
+
 		fillRect(start_x + i * GALTON_H_SEPARATION - GRAPH_BAR_WIDTH/2, GRAPH_START_Y - 100, GRAPH_BAR_WIDTH, 100, BLACK);
-		fillRect(start_x + i * GALTON_H_SEPARATION - GRAPH_BAR_WIDTH/2, GRAPH_START_Y - graph_height, GRAPH_BAR_WIDTH, graph_height, RED);
+		drawRect(start_x + i * GALTON_H_SEPARATION - GRAPH_BAR_WIDTH/2, GRAPH_START_Y - graph_height, GRAPH_BAR_WIDTH, graph_height, RED);
 	}
 }
 
+// Reset the contents of the graph
+void resetGraph(void)
+{
+	for (int i = 0; i < GRAPH_LEN; i++)
+	{
+		graph[i] = 0;
+	}
+
+	num_fallen_through = 0;
+
+	clearGraph();
+}
+
+// Based on the potentiometer mode and value, update the simulation
+void update_potentiometer(void)
+{
+	uint16_t pot_value = readPotentiometer();
+
+	// Make sure the potentiometer has deviated enough for a valid update
+	if (abs(pot_last_value - (int)pot_value) < POT_MIN_DEVIATION)
+	{
+		return;
+	}
+
+	pot_last_value = pot_value;
+
+	if (pot_value > POT_MAX)
+	{
+		pot_value = POT_MAX;
+	}
+
+	switch (mode)
+	{
+		case BUTTON_MODE_NUM_BALLS:
+			pot_value = (int) (((float) pot_value / (float) POT_MAX) * ((float) MAX_BALLS));
+			setNumBalls(pot_value);
+			break;
+
+		case BUTTON_MODE_GRAVITY:
+			ball_gravity = float2fix15(((float) pot_value / (float) POT_MAX) * (MAX_GRAVITY - MIN_GRAVITY) + MIN_GRAVITY);
+			break;
+
+		case BUTTON_MODE_BOUNCINESS:
+			ball_bounciness = float2fix15(((float) pot_value / (float) POT_MAX) * (MAX_BOUNCINESS - MIN_BOUNCINESS) + MIN_BOUNCINESS);
+			break;
+		
+		case BUTTON_MODE_NOTHING:
+			break;
+	}
+
+	num_fallen_through = 0;
+	resetGraph();
+}
+
+// Play the ball sound
+void play_hit_sound(void)
+{
+	dma_channel_start(ctrl_chan);
+}
+
+// Find the nearest peg to a ball
+peg_t *findPeg(ball_t *ball)
+{
+	// Find the row by dividing the ball's Y-coordinate by the Y separation of the pegs, relative to the top of the board
+	int temp_y = fix2int15(ball->y);
+
+	temp_y -= GALTON_TOP_Y;
+	temp_y += GALTON_V_SEPARATION/2;
+	
+	int row = temp_y / GALTON_V_SEPARATION;
+
+	// If outside of the board's y bounds, return no peg
+	if (row < 0 || row >= GALTON_HEIGHT)
+	{
+		return NULL;
+	}
+
+	// Find the column by dividing the ball's X-coordinate by the X separation of the pegs, relative to the leftmost peg of the row
+	int temp_x = ((row + 1) * GALTON_H_SEPARATION) / 2;
+	int start_x = 320 - temp_x;
+	int end_x = 320 + temp_x;
+	
+	// If outside of the row's x bounds, return no peg
+	if (fix2int15(ball->x) > end_x || fix2int15(ball->x) < start_x)
+	{
+		return NULL;
+	}
+
+	temp_x = fix2int15(ball->x) - start_x;
+	int col = temp_x / GALTON_H_SEPARATION;
+
+	// Return the peg at the calculated index
+	int index = row * (row + 1) / 2 + col;
+	return &pegs[index];
+}
+
+// Update a ball's position and velocity, and detect any bounces. 
 void updateBall(ball_t *ball)
 {
 	fix15 dx; 
@@ -301,45 +505,55 @@ void updateBall(ball_t *ball)
 	fix15 temp;
 	peg_t *peg;
 
-	for (int i = 0; i < pegs_len; i++)
+	// Find the nearest peg
+	peg = findPeg(ball);
+
+	if (peg != NULL)
 	{
-		peg = &pegs[i];
 		dx = ball->x - peg->x;
 		dy = ball->y - peg->y;
 
 		// check if the ball is overlapping the peg
-		if (fix2float15(absfix15(dx)) >= BALL_RADIUS + PEG_RADIUS || fix2float15(absfix15(dy)) >= BALL_RADIUS + PEG_RADIUS)
+		if (absfix15(dx) < BALL_RADIUS_FIX + PEG_RADIUS_FIX && absfix15(dy) < BALL_RADIUS_FIX + PEG_RADIUS_FIX)
 		{
-			continue;
-		}
+			// Use Alpha-max beta-min to estimate the distance
+			fix15 absdx = absfix15(dx);
+			fix15 absdy = absfix15(dy);
 
-		dist = float2fix15((float) sqrt((double) fix2float15(multfix15(dx, dx) + multfix15(dy, dy))));
+			fix15 max = MAX(absdx, absdy);
+			fix15 min = MIN(absdx, absdy);
 
-		normal_x = divfix(dx, dist);
-		normal_y = divfix(dy, dist);
+			dist = max + divfix(min, int2fix15(4));
 
-		temp = multfix15(int2fix15(-2), (multfix15(normal_x, ball->vx) + multfix15(normal_y, ball->vy)));
+			// Calculate the new velocity
+			normal_x = divfix(dx, dist);
+			normal_y = divfix(dy, dist);
 
-		if (temp > 0)
-		{
-			ball->x = peg->x + multfix15(normal_x, dist + int2fix15(1));
-			ball->y = peg->y + multfix15(normal_y, dist + int2fix15(1));
+			temp = multfix15(int2fix15(-2), (multfix15(normal_x, ball->vx) + multfix15(normal_y, ball->vy)));
 
-			ball->vx = ball->vx + multfix15(normal_x, temp);
-			ball->vy = ball->vy + multfix15(normal_y, temp);
-
-			if (i != ball->last_peg)
+			if (temp > 0)
 			{
-				ball->last_peg = i;
+				ball->x = peg->x + multfix15(normal_x, dist + int2fix15(1));
+				ball->y = peg->y + multfix15(normal_y, dist + int2fix15(1));
 
-				play_hit_sound();
+				ball->vx = ball->vx + multfix15(normal_x, temp);
+				ball->vy = ball->vy + multfix15(normal_y, temp);
 
-				ball->vx = multfix15(ball->vx, float2fix15(BALL_BOUNCINESS));
-				ball->vy = multfix15(ball->vy, float2fix15(BALL_BOUNCINESS));
+				// If hitting a new peg, play a hit sound
+				if (peg != ball->last_peg)
+				{
+					ball->last_peg = peg;
+
+					play_hit_sound();
+
+					ball->vx = multfix15(ball->vx, ball_bounciness);
+					ball->vy = multfix15(ball->vy, ball_bounciness);
+				}
 			}
 		}
 	}
 
+	// Check if the balls are hitting the edge of the graph display and update their motion accordingly
 	if (hitBottom(ball->y))
 	{
 		updateGraph(ball);
@@ -365,12 +579,14 @@ void updateBall(ball_t *ball)
 		ball->x  = (ball->x + int2fix15(5));
 	} 
 
-	ball->vy += float2fix15(BALL_GRAVITY);
+	// Update the ball's velocity
+	ball->vy += ball_gravity;
 
 	ball->x += ball->vx;
 	ball->y += ball->vy;
 }
 
+// Spawn a new peg at the given position
 void spawnPeg(int x, int y)
 {
 	peg_t *peg = &pegs[pegs_len];
@@ -379,6 +595,7 @@ void spawnPeg(int x, int y)
 	pegs_len += 1;
 }
 
+// Create a galton board
 void createGalton()
 {
 	int startx = 0;
@@ -399,7 +616,7 @@ void drawBoard()
 {
 	for (int i = 0; i < pegs_len; i++)
 	{
-		fillCircle(fix2int15(pegs[i].x), fix2int15(pegs[i].y), 6, BLUE);
+		drawCircle(fix2int15(pegs[i].x), fix2int15(pegs[i].y), 6, BLUE);
 	}
 
 	drawVLine(SCREEN_RIGHT_X, SCREEN_TOP_Y, SCREEN_BOTTOM_Y - SCREEN_TOP_Y, WHITE);
@@ -413,9 +630,11 @@ void drawBoard()
 	
 	static char str[256];
 
-	sprintf(str, "Ball count:       %lu    \nFallen through:   %lu\nTime since boot:  %us", balls_len, num_fallen_through, (int) time_since_boot);
+	sprintf(str, "Ball count:       %lu    \nBounciness:       %.02f  \nGravity:          %.02f     \nFallen through:   %lu   \nTime since boot:  %us   \nButton mode:      %s\n",
+		 balls_len, fix2float15(ball_bounciness), fix2float15(ball_gravity), num_fallen_through, (int) time_since_boot,
+		 button_mode_to_str(mode));
 
-	// fillRect(10, 10, 200, 100, BLACK);
+	// drawRect(10, 10, 200, 100, BLACK);
 	writeString(str);
 }
 
@@ -470,7 +689,6 @@ static PT_THREAD (protothread_anim(struct pt *pt))
 	{
 		spawnball();
 	}
-	// Spawn a ball
 
 	createGalton();
 
@@ -480,13 +698,10 @@ static PT_THREAD (protothread_anim(struct pt *pt))
 		// Measure time at start of thread
 		begin_time = time_us_32();    
 		
-		// clearLowFrame(0, BLACK);
-
-
 		for (int i = 0; i < balls_len; i++)
 		{
 			// erase ball
-			fillCircle(fix2int15(balls[i].x), fix2int15(balls[i].y), BALL_RADIUS, BLACK);
+			drawCircle(fix2int15(balls[i].x), fix2int15(balls[i].y), BALL_RADIUS_INT, BLACK);
 		}
 
 		update_potentiometer();
@@ -496,7 +711,8 @@ static PT_THREAD (protothread_anim(struct pt *pt))
 			// update ball's position and velocity, check for peg hits
 			updateBall(&balls[i]);
 
-			fillCircle(fix2int15(balls[i].x), fix2int15(balls[i].y), BALL_RADIUS, GREEN);
+			drawCircle(fix2int15(balls[i].x), fix2int15(balls[i].y), BALL_RADIUS_INT, GREEN);
+			// drawRect(fix2int15(balls[i]->x) - BALL_RADIUS_INT, fix2int15(balls[i]->y) - BALL_RADIUS_INT, BALL_RADIUS_INT * 2 + 1, BALL_RADIUS_INT * 2 + 1, GREEN);
 			// draw the ball at its new position
 		}
 
@@ -506,10 +722,15 @@ static PT_THREAD (protothread_anim(struct pt *pt))
 		drawGraph();
 
 		readPotentiometer();
+		
+		update_button_state();
 
 		// delay in accordance with frame rate
 		spare_time = FRAME_RATE - (time_us_32() - begin_time);
 		// yield for necessary amount of time
+
+		gpio_put(LED, spare_time < 0);
+
 		PT_YIELD_usec(spare_time);
 		time_since_boot += ((float) FRAME_RATE) / ((float) 1000000);
 		// NEVER exit while
@@ -563,8 +784,15 @@ int main()
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
 
+	gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+    gpio_put(LED, 0);
+
 	adc_gpio_init(PIN_POT);
 	adc_select_input(1);
+
+	gpio_set_dir(BUTTON_PIN, false);
+	gpio_pull_up(BUTTON_PIN);
 
     // Build sine table and DAC data table
     int i;
